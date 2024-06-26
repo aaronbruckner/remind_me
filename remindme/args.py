109a62b75371d7b189b67fe909d03b313cccc234
Aaron Bruckner <aaronbruckner@gmail.com>
2024-06-25 18:17:17 -0700

    Cleaned up help docs.

--- remindme/args.py
+++ remindme/args.py
@@ -23,13 +23,30 @@ class LoveParser(argparse.ArgumentParser):
 def parse_command_line_arguments():
     invoke = "./remindme.py"
     usage = f"""
-    [usage_comment]# A good starting point! Prints tool statistics.[/]
-    [usage_comment]# Don't forget to include a password or love may not be decryptable.[/]
-    [usage_code]{invoke} about -pw myPassword[/]
+    [usage_code]{invoke} stats -pw myPassword[/]
+    [usage_code]{invoke} love -pw myPassword[/]
+    [usage_code]{invoke} story -pw myPassword[/]
+    [usage_code]{invoke} sex -pw myPassword[/]
     """
     parser = LoveParser(
-        description="[red]A utility written for Miranda to reminder her about all the ways I love her[/] ❤️",
-        usage=usage
+        description="""\
+        [red]A utility written for Miranda to reminder her about all the ways I love her[/] ❤️
+        Pulls encrypted data from online, decrypts it, displays one of the selected topics, and tracks progress.
+
+        Log back in occasionally to see new submissions!
+
+        Progress is saved in current working directory under ./remindme_progress
+        """,
+        usage=usage,
+        formatter_class=argparse.RawTextHelpFormatter
     )
-    parser.add_argument("action", help="Specify")
+    action_description = """\
+    specify a primary action for the script. Allowed values:
+      * [bold red]stats[/] - Provides basic summary of the tool along with progress in each category.
+      * [bold red]love[/] - Prints a love-based reminder. Widest category of topics.
+      * [bold red]story[/] - Prints a story-based reminder recalling some of my favorite memories with you.
+      * [bold red]sex[/] - Prints a sex/body-based reminder.
+    """
+    parser.add_argument("action", help=action_description, choices=["stats", "love", "story", "sex"])
+    parser.add_argument("-pw", required=True, help="Love cannot defeat symmetric encryption. Ask Aaron for the password ❤️")
     parser.parse_args()
